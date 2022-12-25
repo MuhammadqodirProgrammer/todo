c26e93bada96d653d54382c07e9c533c598237e8
MuhammadqodirProgrammer <komilovm831@gmail.com>
2022-12-25 23:31:02 +0500

    dark-line

--- js/main.js
+++ js/main.js
@@ -5,11 +5,38 @@ const elInput =document.querySelector('.js-input')
 const elbtn =document.querySelector('.js-btn')
 const elList =document.querySelector('.list')
 const elbtngroup =document.querySelector('.btn-group')
-// const elButtonElDelete =document.querySelector('.js-delate-btn')
+const comletedspan =elbtngroup.children[1].children[0]
+const unComletedspan =elbtngroup.children[2].children[0]
+const allBtn =elbtngroup.children[0]
+const allDelade =document.querySelector('.js-delateall')
+const localData =JSON.parse(window.localStorage.getItem('todos'))
 
-const todos =[]
+const elMode =document.querySelector('.js-mode')
+let theme =false
+elMode.addEventListener('click', ()=>{
+    theme =!theme
+    console.log(theme);
+    const bg =theme ? 'dark':'light';
+    window.localStorage.setItem('theme',bg)
+    ChangeTheme()
+})
+function ChangeTheme() {
+    if (window.localStorage.getItem("theme" ) == "dark") {
+        document.body.classList.add('dark')
+    }else{
+        document.body.classList.remove('dark')
+    }
+}
+ChangeTheme()
 
+// const todos =localData ? localData :[];
+const todos =localData || [];
+
+rendertodos(todos,elList)
 function rendertodos(arr,node){
+    window.localStorage.setItem('todos', JSON.stringify(todos))
+    comletedspan.textContent =todos.filter((el) =>el.isCompleted).length
+  unComletedspan.textContent =todos.filter((el) =>el.isCompleted !==true).length
     node.innerHTML =""
 elInput.value =""
     arr.forEach((item)=>{
@@ -40,7 +67,7 @@ buttonElEdit.textContent = "Edit"
     newitem.appendChild(newSpan)
     newitem.appendChild(buttonElEdit)
     newitem.appendChild(buttonElDelete)
-    // console.log(item.isCompleted);
+    
 if(item.isCompleted){
     newInput.checked =true
     newSpan.style.textDecoration = 'line-through'
@@ -51,15 +78,23 @@ if(item.isCompleted){
  node.appendChild(newitem)
  elbtngroup.children[0].children[0].innerHTML =todos.length
     })
+ 
 }
+const elH3 =document.createElement('h2')
+elH3.classList.add('text-center')
+elList.appendChild(elH3)
 
+if (todos.length == 0) {
+    elH3.textContent ="TODOLAR YO'Q ☹"
+    
+}
 
 elForm.addEventListener("submit",(evt) =>{
     evt.preventDefault()
     
    
     const todosobj = {
-                id:todos.length + 1 ,
+                id:todos.length ? todos[todos.length - 1].id + 1 : 1 ,
                 text:elInput.value,
                 isCompleted:false
                 }
@@ -69,9 +104,31 @@ rendertodos(todos,elList)
 
 })
 let sum =0
+const newtodos =[]
+const newtodosun =[]
+const Completed =elbtngroup.children[1]
+const ComUn =elbtngroup.children[2]
+
+allBtn.addEventListener('click',(evt)=>{
+    evt.preventDefault()
+    rendertodos(todos,elList)
+})
+
+Completed.addEventListener('click',(evt)=>{
+    evt.preventDefault()
+    const filtred = todos.filter((el) =>el.isCompleted ==true)
+    console.log(filtred);
+    rendertodos(filtred,elList)
+})
+ComUn.addEventListener('click',(evt)=>{
+    evt.preventDefault()
+    const filtred = todos.filter((el) =>el.isCompleted !==true)
+    console.log(filtred);
+    rendertodos(filtred,elList)
+})
+
 
 elList.addEventListener('click',(evt)=>{
-    // console.log(evt.target.matches('.js-delate-btn'));
     if(evt.target.matches('.js-delate-btn')){
         const todoId =evt.target.dataset.todoId;
         const findindex =todos.findIndex((item) =>item.id == todoId)
@@ -90,49 +147,21 @@ rendertodos(todos,elList)
         const todoId =evt.target.dataset.todoId;
      const findentItem =todos.find((item) => item.id == todoId)
      findentItem.isCompleted =!findentItem.isCompleted
-     if(findentItem.isCompleted){
-    let sumb =++sum
- elbtngroup.children[1].children[0].innerHTML =sumb
- let unsum =todos.length - sumb
- elbtngroup.children[2].children[0].innerHTML = unsum
-}
-const newtodos =[]
-const newtodosun =[]
-const Completed =elbtngroup.children[1]
-const ComUn =elbtngroup.children[2]
-console.log(elbtngroup.children[2]);
-
-Completed.addEventListener('click',(evt)=>{
-    evt.preventDefault()
-    todos.forEach((el) =>{
-        if(el.isCompleted){
-      newtodos.push(el)
-            console.log(el);
-        }
-    })
-    rendertodos(newtodos,elList)
-    console.log(newtodos);
-})
-
-console.log(ComUn);
-ComUn.addEventListener('click',(evt)=>{
-    evt.preventDefault()
-    todos.forEach((el) =>{
-        if(el.isCompleted !=true){
-      newtodosun.push(el)
-            console.log(el);
-        }
-        console.log(newtodosun);
-        
-    })
-    rendertodos(newtodosun,elList)
-})
-
-
-console.log(Completed);
 
 rendertodos(todos,elList)
     }
+})
+
+
+allDelade.addEventListener('click',(evt)=>{
+    if(evt.target.matches('.js-delateall')){
+        window.localStorage.removeItem('todos') ; 
+
+elList.setAttribute('class','text-center size')
+
+rendertodos(todos,elList)
+elList.innerHTML ="TODOLAR O'CHIRILDI 🧐"
+    };
 })
 
 
